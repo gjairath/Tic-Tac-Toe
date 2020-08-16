a031dd6abc1fa3b1083317eb407a9876b4e315b1
ajay jairath <gjairath@purdue.edu>
2020-08-16 00:58:08 -0400

    Added file to remember debugging

---
+++ TODO.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug 16 00:52:07 2020
+
+@author: garvi
+"""
+
+
+#TODO
+#it's creating a new board instance and thats causing issues with q-table, i need to hash it. 
+#(If you forget, try printing self.q, you'll see how arrays are same board addresses different)
